eb83f65a2d451f78c3dd621ab3b65c56e8d0435b
ptstp <animalfusion88@gmail.com>
2021-06-21 20:00:48 +0300

    add directory file read & analyze

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import KeyDetect
 import os
 
 
-
+directory = "./audio"
+files = os.listdir(directory)
 
 dict = {'C Maj':'8B','C# Maj':'3B','D Maj':'10B','D# Maj':'5B','E Maj':'12B','F Maj':'7B','F# Maj':'2B','G Maj':'9B','G# Maj':'4B','A Maj':'11B','A# Maj':'6B','B Maj':'1B','c min':'5A','c# min':'12A','d min':'7A','d# min':'2A','e min':'9A','f min':'4A','f# min':'11A','g min':'6A','g# min':'1A','a min':'8A','a# min':'3A','b min':'10A'}
 
-key = KeyDetect.keyDetect("test2.mp3")
-print(dict[key])
+print(files)
+
+for i in range(len(files)):
+    trackName = files[i]
+    trackNames = trackName.split('.')
+    format = trackNames[-1]
+    if format == 'mp3' or format == 'wav':
+        path = directory + '/' + trackName
+        key = KeyDetect.keyDetect(path)
+        print(dict[key] + ' ' + key + '' + trackName)
 
 
 
